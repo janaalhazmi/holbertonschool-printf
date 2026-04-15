7c82ce36470d30dea6b650959567a9cbc8b04baa
janaalhazmi <janaalhazmi78@gmail.com>
2026-04-15 14:49:49 +0000

    task

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <unistd.h>
+
+/* main */
+int _printf(const char *format, ...);
+
+/* helpers */
+int print_char(char c);
+int print_string(char *str);
+int print_int(int n);
+
+#endif
