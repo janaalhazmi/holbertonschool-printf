522a3c88f9fa74e606f9be3d9a847a2727f501d8
laila-khalid”git config --global user.email 13252@holbertonstudents.comgit config --global user.name laila-khalid” <13252@holbertonstudents.com>
2026-04-16 16:59:19 +0000

    Add tests directory with printf_our_tests.c and update README

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # holbertonschool-printf
+## Testing
+To ensure the robustness of our `_printf` implementation, we conducted manual testing using a custom test suite. 
+
+You can find our test cases in:
+* `tests/printf_our_tests.c`
+
+To run the tests locally:
+`gcc -Wall -Werror -Wextra -pedantic -std=gnu89 tests/printf_our_tests.c *.c -o test_output && ./test_output`
---
+++ tests/printf_our_tests.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point for testing _printf
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	_printf("--- Task 0 Tests ---\n");
+	_printf("Character: [%c]\n", 'H');
+	_printf("String: [%s]\n", "Holberton School");
+	_printf("Percent: [%%]\n");
+
+	_printf("\n--- Task 1 Tests (Integers) ---\n");
+	_printf("Positive Number: [%d]\n", 98);
+	_printf("Negative Number: [%d]\n", -1024);
+	_printf("Zero: [%i]\n", 0);
+
+	return (0);
+}
